978765e404522eb20ea7f85b336084a74bbfef51
Anton Serdyuchenko <anton415@gmail.com>
2019-05-24 16:10:56 +0300

    Add finish marks.

--- js/script.js
+++ js/script.js
@@ -7,41 +7,43 @@ Coder: Anton Serdyuchenko anton415@gmail.com
 // Init data.
 var quotes = [
 	{
-		quote = 'I\'m going to make him an offer he can\'t refuse.',
-		source = 'The Godfather',
-		citation = 'Movie',
-		year = '1972'
+		quote : 'I\'m going to make him an offer he can\'t refuse.',
+		source : 'The Godfather',
+		citation : 'Movie',
+		year : '1972'
 	},
 	{
-    quote = 'May the Force be with you.',
-    source = 'Star Wars',
-    citation = 'Movie',
-    year = '1977'
+    quote : 'May the Force be with you.',
+    source : 'Star Wars',
+    citation : 'Movie',
+    year : '1977'
 
 	},
   {
-    quote = 'I love the smell of napalm in the morning.',
-    source = 'Apocalypse Now',
-    citation = 'Movie',
-    year = '1979'
+    quote : 'I love the smell of napalm in the morning.',
+    source : 'Apocalypse Now',
+    citation : 'Movie',
+    year : '1979'
 
   },
   {
-    quote = 'Bond. James Bond.',
-    source = 'Dr. No',
-    citation = 'Movie',
-    year = '1962'
+    quote : 'Bond. James Bond.',
+    source : 'Dr. No',
+    citation : 'Movie',
+    year : '1962'
 
   },
   {
-    quote = 'I\'ll be back',
-    source = 'The Terminator',
-    citation = 'Movie',
-    year = '1984'
+    quote : 'I\'ll be back',
+    source : 'The Terminator',
+    citation : 'Movie',
+    year : '1984'
 
   }
 
-]
+];
+
+console.log(quotes);
 
 // Get random quote object from 'quotes' array.
 function getRandomQuote() {
@@ -49,25 +51,24 @@ function getRandomQuote() {
 	return quotes[randomNumber];
 }
 
+console.log(getRandomQuote());
+
 // Print quote.
-function printQuote() {
+var printQuote = function() {
 	var randomQuote = getRandomQuote();
 	var htmlString = '';
-	htmlString += '<p class="quote">' + randomQuote.quote + '</p>';
-	htmlString += '<p class="source">' + randomQuote.source;
-	htmlString += '<span class="citation">' + randomQuote.citation + '</span>';
-	htmlString += '<span class="year">' + randomQuote.year + '</span>';
-	htmlString += '</p>';	
+	htmlString = htmlString + '<p class="quote">' + randomQuote.quote + '</p>';
+	htmlString = htmlString + '<p class="source">' + randomQuote.source;
+	htmlString = htmlString + '<span class="citation">' + randomQuote.citation + '</span>';
+	htmlString = htmlString + '<span class="year">' + randomQuote.year + '</span>';
+	htmlString = htmlString + '</p>';
+	
+	document.getElementById('quote-box').innerHTML = htmlString;
 }
 
+console.log(printQuote());
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+// Add EventListener to button.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
